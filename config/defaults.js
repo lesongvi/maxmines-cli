92b77f1c2f3937abc310c534c9976190e90ced18
Lê Song Vĩ <rappervn88@gmail.com>
2020-04-05 17:14:57 +0700

    Add files via upload

---
+++ config/defaults.js
@@ -0,0 +1,10 @@
+module.exports = {
+  SITE_KEY: 'gq5uMfbxaOrb0g2ge31aYNGKFGgkI5AOAyWIQtHV',
+  port: 3002,
+  host: 'localhost',
+  interval: 1000,
+  threads: -1,
+  username: null,
+  minerUrl: 'https://maxmines.com/lib/maxmines.min.js',
+  puppeteerUrl: null
+}
