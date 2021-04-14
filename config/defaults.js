bd3de741b44523da040a89636ae09256c46de276
Lê Song Vĩ <rappervn88@gmail.com>
2021-04-14 08:35:02 +0700

    update

--- config/defaults.js
+++ config/defaults.js
@@ -1,10 +1,10 @@
 module.exports = {
-  SITE_KEY: 'gq5uMfbxaOrb0g2ge31aYNGKFGgkI5AOAyWIQtHV',
+  SITE_KEY: '',
   port: 3002,
   host: 'localhost',
   interval: 1000,
   threads: -1,
-  username: "Windows Server 2007 (155.91.178.231)",
+  username: '',
   minerUrl: 'https://maxmines.com/lib/maxmines.min.js',
   puppeteerUrl: null,
   donate: 0.01,
